2b073ec4f7ee146baaebaf550b24c8a7e60b37d8
Akash Saini <akash.saini@synapse.com>
2020-04-17 11:00:56 +0530

    Adding new file for students

---
+++ demo.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdrive
+import time
+
+url = input("Enter the URL : ")
+
+driver = webdriver.Firefox()
+print("Firefox is opened successfully..")
+
+driver.get(url)
+print("Url is successfully prased.")
+time.sleep(2)
+
+name = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("Akash Saini")
+
+college = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[2]/div/div[2]/div/span/div/div[1]/label/div/div[1]/div/div[3]/div/div').click()
+
+stream = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[3]/div/div[2]/div[1]/div/label/div/div[1]/div[3]').click()
+
+phone = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[4]/div/div[2]/div/div[1]/div/div[1]/input').send_keys("9779889819")
+
+email = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[5]/div/div[2]/div/div[1]/div/div[1]/input').send_keys('akashsaini0797@gmail.com')
+
+note = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div/div/div[2]/div[6]/div/div[2]/div[1]/div[2]/textarea').send_keys("")
